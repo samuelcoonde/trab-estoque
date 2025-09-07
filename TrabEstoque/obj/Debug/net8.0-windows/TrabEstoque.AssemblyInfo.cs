dc1d8dec8c7f06325454f2539eee2de9af98ec8c
samuel <samuelconnde@gmail.com>
2025-09-06 21:34:50 -0300

    Adicionando form para tela de início

--- TrabEstoque/obj/Debug/net8.0-windows/TrabEstoque.AssemblyInfo.cs
+++ TrabEstoque/obj/Debug/net8.0-windows/TrabEstoque.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TrabEstoque")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28944771456e137761390d90d789a874b0b67156")]
 [assembly: System.Reflection.AssemblyProductAttribute("TrabEstoque")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TrabEstoque")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
